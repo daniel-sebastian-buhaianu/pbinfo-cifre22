e03792cd33ea3fba42b4c05a6ca98ebd0cd6f787
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-08-24 06:26:44 +0300

    Update cifre22.c

--- cifre22.c
+++ cifre22.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 
+int main()
+{
+	unsigned short n, i, cif;
+	unsigned x, aux, nrmax = 0, asocparx, asocparmax = 0, p;
+
+	scanf("%hu", &n);
+
+	for (i = 0; i < n; i++) {
+		scanf("%u", &x);
+		
+		aux = x, asocparx = 0, p = 1;
+
+		while (aux) {
+			cif = aux % 10;
+
+			if (cif % 2 == 0) {
+				asocparx = asocparx + p * cif;
+				p = p * 10;
+			}
+
+			aux /= 10;
+		}
+
+		if (asocparmax < asocparx) {
+			asocparmax = asocparx;
+			nrmax = x;
+		} else if (asocparmax == asocparx) {
+			if (x > nrmax) {
+				nrmax = x;
+			}
+		}
+		//printf("x = %u, asocparx = %u, asocparmax = %u, nrmax = %u\n", x, asocparx, asocparmax, nrmax);
+	}
+
+	printf("%u", nrmax);
+
+	return 0;
+}
+// scor 100
